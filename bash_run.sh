c4a91689fd37cec9c00d32728e21c1ef0ae28c5e
Rhuan Lima <rhuan.lima@gmail.com>
2021-10-17 21:31:54 +0000

    update git ignore and fix sh

--- bash_run.sh
+++ bash_run.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-cd ~/git_projects/twitch-telegram/
-source ~/git_projects/twitch-telegram/.venv/bin/activate
-python app.py
+#!/bin/bash
+cd /home/ubuntu/git_projects/twitch-telegram/
+source /home/ubuntu/git_projects/twitch-telegram/.venv/bin/activate
+python /home/ubuntu/git_projects/twitch-telegram/app.py
 deactivate
